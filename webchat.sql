776dbdf59e15714461d44419fbab1d52caf8ee57
flouritess <302618914@qq.com>
2019-06-22 00:14:21 +0800

    最新数据库

---
+++ webchat.sql
@@ -0,0 +1,173 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 123
+Source Server Version : 50720
+Source Host           : localhost:3306
+Source Database       : webchat
+
+Target Server Type    : MYSQL
+Target Server Version : 50720
+File Encoding         : 65001
+
+Date: 2019-06-22 00:13:22
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `admin`
+-- ----------------------------
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin` (
+  `user_id` int(11) NOT NULL,
+  KEY `id` (`user_id`),
+  CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of admin
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `broadcast`
+-- ----------------------------
+DROP TABLE IF EXISTS `broadcast`;
+CREATE TABLE `broadcast` (
+  `broadcast_id` int(11) NOT NULL,
+  `broadcast_content` varchar(255) DEFAULT NULL,
+  `broadcast_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`broadcast_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of broadcast
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `friends`
+-- ----------------------------
+DROP TABLE IF EXISTS `friends`;
+CREATE TABLE `friends` (
+  `user_id` int(11) NOT NULL,
+  `friend_id` int(11) NOT NULL,
+  `add_time` datetime NOT NULL,
+  KEY `user_id` (`user_id`),
+  KEY `friend_id` (`friend_id`),
+  CONSTRAINT `friends_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `friends_ibfk_2` FOREIGN KEY (`friend_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of friends
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `message`
+-- ----------------------------
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE `message` (
+  `message_id` int(11) NOT NULL,
+  `content` varchar(255) DEFAULT NULL,
+  `message_type` int(11) DEFAULT NULL,
+  `receiver_id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  PRIMARY KEY (`message_id`),
+  KEY `receiver_id` (`receiver_id`),
+  KEY `sender_id` (`sender_id`),
+  KEY `message_type` (`message_type`),
+  CONSTRAINT `message_ibfk_1` FOREIGN KEY (`receiver_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `message_ibfk_2` FOREIGN KEY (`sender_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `message_ibfk_3` FOREIGN KEY (`message_type`) REFERENCES `message_type` (`messagae_type_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of message
+-- ----------------------------
+INSERT INTO `message` VALUES ('2060235459', 'aaa', '3', '2', '1');
+INSERT INTO `message` VALUES ('2060401072', 'aaa', '3', '2', '1');
+
+-- ----------------------------
+-- Table structure for `message_type`
+-- ----------------------------
+DROP TABLE IF EXISTS `message_type`;
+CREATE TABLE `message_type` (
+  `messagae_type_id` int(11) NOT NULL,
+  `message_type` varchar(32) NOT NULL,
+  PRIMARY KEY (`messagae_type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of message_type
+-- ----------------------------
+INSERT INTO `message_type` VALUES ('1', '聊天消息');
+INSERT INTO `message_type` VALUES ('2', '系统消息');
+INSERT INTO `message_type` VALUES ('3', '好友消息');
+
+-- ----------------------------
+-- Table structure for `normal_user`
+-- ----------------------------
+DROP TABLE IF EXISTS `normal_user`;
+CREATE TABLE `normal_user` (
+  `user_id` int(11) NOT NULL,
+  `gender` varchar(16) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  KEY `id` (`user_id`),
+  CONSTRAINT `normal_user_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of normal_user
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `sercurity_log`
+-- ----------------------------
+DROP TABLE IF EXISTS `sercurity_log`;
+CREATE TABLE `sercurity_log` (
+  `log_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `login_time` datetime DEFAULT NULL,
+  `login_address` varchar(255) NOT NULL,
+  PRIMARY KEY (`log_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `sercurity_log_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sercurity_log
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `system`
+-- ----------------------------
+DROP TABLE IF EXISTS `system`;
+CREATE TABLE `system` (
+  `system_id` int(11) NOT NULL,
+  `salt` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`system_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of system
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(16) DEFAULT NULL,
+  `nickname` varchar(255) DEFAULT NULL,
+  `imgurl` varchar(255) DEFAULT NULL,
+  `tel` varchar(16) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', 'sss', 'shan', null, null, '123');
+INSERT INTO `user` VALUES ('2', 'xxx', 'wen', null, null, '123');
