47048e22aa397859771c8281738c4e2d69996b2c
Everythingtomyheart <2507546020@qq.com>
2019-06-25 18:37:58 +0800

    mapper修改

--- src/main/java/com/gabe/mychat/mapper/messageUtilMapper.java
+++ src/main/java/com/gabe/mychat/mapper/messageUtilMapper.java
@@ -2,10 +2,11 @@ package com.gabe.mychat.mapper;
 
 import com.gabe.mychat.pojo.message;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
 public interface messageUtilMapper {
-    List<message> selectByTime(String senderId,String receiverId);
+    List<message> selectByTime(@Param("senderId") String senderId,@Param("receiverId")  String receiverId);
 }
