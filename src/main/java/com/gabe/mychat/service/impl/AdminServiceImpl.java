e1fdc26bb5b2faaecdea96c0c2909165d55cb5e6
flouritess <302618914@qq.com>
2019-06-27 15:00:36 +0800

    redis更新

--- src/main/java/com/gabe/mychat/service/impl/AdminServiceImpl.java
+++ src/main/java/com/gabe/mychat/service/impl/AdminServiceImpl.java
@@ -7,13 +7,14 @@ import com.gabe.mychat.pojo.user;
 import com.gabe.mychat.pojo.userExample;
 import com.gabe.mychat.service.AdminService;
 import com.gabe.mychat.util.PerfectUser;
-import com.gabe.mychat.util.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * description:
@@ -32,37 +33,25 @@ public class AdminServiceImpl implements AdminService {
     @Autowired
     normalUserMapper normalUserMapper;
 
-    @Autowired
-    RedisUtil redisUtil;
     @Override
-    /*@Cacheable(value ="findAllUser")*/
+   /* @Cacheable(value ="findAllUser")*/
     public List<PerfectUser> findAllUser(int status) {
-        List<PerfectUser> perfectUserList=null;
-        //从缓存中拿取
-        List quarterlist=redisUtil.lRange("quarterlist",0,-1);
-        if(quarterlist.size()==0){
-            userExample userExample = new userExample();
-            if (status != 2) {
-                userExample.createCriteria().andStatusEqualTo(status);
-            }
-            List<user> userList = userMapper.selectByExample(userExample);
-            normalUserExample normalUserExample = new normalUserExample();
-            List<normalUser> normalUserList = normalUserMapper.selectByExample(normalUserExample);
-            perfectUserList = new ArrayList<>();
-            for (user user : userList) {
-                for (normalUser normalUser : normalUserList) {
-                    if (normalUser.getUserId().equals(user.getUserId())) {
-                        perfectUserList.add(new PerfectUser(user, normalUser));
-                        break;
-                    }
+        userExample userExample = new userExample();
+        if (status != 2) {
+            userExample.createCriteria().andStatusEqualTo(status);
+        }
+        List<user> userList = userMapper.selectByExample(userExample);
+        normalUserExample normalUserExample = new normalUserExample();
+        List<normalUser> normalUserList = normalUserMapper.selectByExample(normalUserExample);
+        List<PerfectUser> perfectUserList = new ArrayList<>();
+        for (user user : userList) {
+            for (normalUser normalUser : normalUserList) {
+                if (normalUser.getUserId().equals(user.getUserId())) {
+                    perfectUserList.add(new PerfectUser(user, normalUser));
+                    break;
                 }
             }
-            redisUtil.lSetlist("quarterlist",perfectUserList,1);
         }
-        else{
-            return quarterlist;
-        }
-
         return perfectUserList;
     }
 
@@ -138,36 +127,16 @@ public class AdminServiceImpl implements AdminService {
     }
 
     @Override
-   /* @CachePut(value ="findAllUser")*/
     public boolean prohibitUser(String userId) {
-        List quarterlist=redisUtil.lRange("quarterlist",0,-1);
-
         user user = userMapper.selectByPrimaryKey(userId);
         user.setStatus(1);
-        for(int i=0;i<quarterlist.size();i++){
-            PerfectUser PerfectUser=(PerfectUser)quarterlist.get(i);
-            if(PerfectUser.getUserId().equals(user.getUserId())){
-                PerfectUser.setStatus(1);
-            }
-        }
-        redisUtil.lSetlist("quarterlist",quarterlist,1);
         return userMapper.updateByPrimaryKey(user) != 0;
     }
 
     @Override
-   /* @CachePut(value ="findAllUser")*/
     public boolean releaseUser(String userId) {
-        List quarterlist=redisUtil.lRange("quarterlist",0,-1);
-
         user user = userMapper.selectByPrimaryKey(userId);
         user.setStatus(0);
-        for(int i=0;i<quarterlist.size();i++){
-            PerfectUser PerfectUser=(PerfectUser)quarterlist.get(i);
-            if(PerfectUser.getUserId().equals(user.getUserId())){
-                PerfectUser.setStatus(1);
-            }
-        }
-        redisUtil.lSetlist("quarterlist",quarterlist,0);
         return userMapper.updateByPrimaryKey(user) != 0;
     }
 }
